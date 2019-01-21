f2b7e45f3672e5242f56f86128959c00de96d15e
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-21 12:15:07 -0800

    send all

---
+++ SortedListInsert/SortedListInsert/Classes/LinkedList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedListInsert.Classes
+{
+    class LinkedList
+    {
+        public Node Head { get; set; }
+        public Node Current { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public void InsertSorted (int value)
+        {
+            Node node = new Node(value);
+
+;           Current = Head;
+
+            // (edge case) if list is empty, insert node at Head
+            if (Head == null)
+            {
+                Head = node;
+                return;
+            }
+            // (edge case) if smaller than Head, insert node at front
+            else if (Head.Value > node.Value)
+            {
+                node.Next = Head;
+                Head = node;
+                return;
+            }
+
+            // find insert location
+            while ( Current.Next != null && Current.Next.Value < value)
+            {
+                Current = Current.Next;
+            }
+
+            // insert once location found
+            node.Next = Current.Next;
+            Current.Next = node;
+        }
+
+
+        public void PrintList ()
+        {
+            Current = Head;
+            Console.Write("HEAD => ");
+
+            while (Current.Next != null)
+            {
+                Console.Write($"{Current.Value} => ");
+                Current = Current.Next;
+            }
+
+            Console.Write($"{Current.Value} => null ");
+
+        }
+    }
+}
---
+++ SortedListInsert/SortedListInsert/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SortedListInsert.Classes;
+
+namespace SortedListInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            list.InsertSorted(5);
+            list.PrintList();
+
+            Console.ReadLine();
+
+            list.InsertSorted(6);
+            list.PrintList();
+
+            Console.ReadLine();
+
+            list.InsertSorted(4);
+            list.PrintList();
+
+            Console.ReadLine();
+
+            list.InsertSorted(0);
+            list.PrintList();
+
+            Console.ReadLine();
+
+            list.InsertSorted(5);
+            list.PrintList();
+
+            Console.ReadLine();
+        }
+    }
+}
